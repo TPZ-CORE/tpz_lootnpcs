ff9c17785761211db716a73b8407b9fbded60403
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-16 00:10:14 +0200

    Create locales.lua

---
+++ tpz_lootnpcs/locales.lua
@@ -0,0 +1,7 @@
+Locales = {}
+
+Locales = {
+
+    ['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'] = "DevTools / Injection Cheat Found!",
+    ['DEVTOOLS_INJECTION_DETECTED']           = "You have been kicked due to cheating by using DevTools or Injection cheat.",
+}
