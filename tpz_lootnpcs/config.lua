c362dbc92f1c4726a45604a1eac04567fd909622
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-16 00:07:04 +0200

    Create config.lua

---
+++ tpz_lootnpcs/config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+-----------------------------------------------------------
+--[[ Webhooking (Only DevTools - Injection Cheat Logs) ]]--
+-----------------------------------------------------------
+
+Config.Webhooks = {
+    
+    ['DEVTOOLS_INJECTION_CHEAT'] = { -- Warnings and Logs about players who used or atleast tried to use devtools injection.
+        Enabled = false, 
+        Url = "", 
+        Color = 10038562,
+    },
+
+}
+
+-----------------------------------------------------------
+--[[ General ]]--
+-----------------------------------------------------------
