7f5c195a1a22045079f425ce5addbbe48e9d2fa8
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-16 18:17:54 +0200

    Update tp-client_main.lua

--- tpz_lootnpcs/client/tp-client_main.lua
+++ tpz_lootnpcs/client/tp-client_main.lua
@@ -1,37 +1,57 @@
 Citizen.CreateThread(function()
-    repeat Wait(1000) until LocalPlayer.state.IsInSession
+
     while true do
+
         local sleep = 0
+
         local size = GetNumberOfEvents(0)
+
         if size > 0 then
-            for i = 0, size - 1 do
-                local eventAtIndex = GetEventAtIndex(0, i)
-                if eventAtIndex == 1376140891 then --event needed
-                    local view   = exports[GetCurrentResourceName()]:DataViewNativeGetEventData2(0, i, 3)
-                    local entity = view["2"]
-                    if not Citizen.InvokeNative(0x964000D355219FC0, entity) then
-                        local eventDataSize = 3
-                        local eventDataStruct = DataView.ArrayBuffer(128)
-                        eventDataStruct:SetInt32(0, 0)
-                        eventDataStruct:SetInt32(8, 0)
-                        eventDataStruct:SetInt32(16, 0)
-                        local is_data_exists = Citizen.InvokeNative(0x57EC5FA4D4D6AFCA, 0, i, eventDataStruct:Buffer(),
-                            eventDataSize)
-                        if is_data_exists then -- can contiue
-                            if PlayerPedId() == eventDataStruct:GetInt32(0) then
-                                local type = GetPedType(entity)
-                                if type == 4 then
-                                    if Citizen.InvokeNative(0x8DE41E9902E85756, entity) then -- _IS_ENTITY_FULLY_LOOTED
-                                    
-                                        TriggerServerEvent("tpz_lootnpcs:server:reward", entity)
-                                    end
+	
+            for index = 0, size - 1 do
+
+                local event = GetEventAtIndex(0, index)
+
+                if event == 1376140891 then -- EVENT_LOOT_COMPLETE
+
+                    local eventDataSize   = 3 -- for EVENT_LOOT_COMPLETE data size is 3
+                    local eventDataStruct = DataView.ArrayBuffer(8 * eventDataSize) -- buffer must be 8*eventDataSize or bigger
+
+                    eventDataStruct:SetInt32(8 * 0, 0) -- 8*0 offset for 0 element of eventData
+                    eventDataStruct:SetInt32(8 * 1, 0) -- 8*0 offset for 0 element of eventData
+                    eventDataStruct:SetInt32(8 * 2, 0) -- 8*0 offset for 0 element of eventData
+
+                    local is_data_exists = Citizen.InvokeNative(0x57EC5FA4D4D6AFCA,0, index, eventDataStruct:Buffer(), eventDataSize)	-- GET_EVENT_DATA
+
+					if is_data_exists then
+
+                        local looterId          = eventDataStruct:GetInt32(8 * 0)
+                        local LootedEntityModel = eventDataStruct:GetInt32(8 * 1)
+                        local IsLootSuccess     = eventDataStruct:GetInt32(8 * 2)
+
+                        if (PlayerPedId() == looterId) and (not Citizen.InvokeNative(0x964000D355219FC0, LootedEntityModel)) then -- We make sure the looterId is our PlayerId and entity is not animal.
+
+                            local type = GetPedType(LootedEntityModel)
+
+                            if type == 4 then
+
+                                if Citizen.InvokeNative(0x8DE41E9902E85756, LootedEntityModel) then -- _IS_ENTITY_FULLY_LOOTED
+                                
+                                    TriggerServerEvent("tpz_lootnpcs:server:reward", LootedEntityModel)
                                 end
+
                             end
+
                         end
+
                     end
+
                 end
+            
             end
+
         end
+
         Wait(sleep)
     end
 end)
