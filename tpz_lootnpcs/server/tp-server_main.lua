b1ece0b0356f159634aaaa463dcda41decded2f4
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-16 21:31:35 +0200

    Update tp-server_main.lua

--- tpz_lootnpcs/server/tp-server_main.lua
+++ tpz_lootnpcs/server/tp-server_main.lua
@@ -13,6 +13,10 @@ local function GetTableLength(T)
     return count
 end
 
+local function StartsWith(String,Start)
+    return string.sub(String,1,string.len(Start))==Start
+end
+
 local function GetPlayerData(source)
 	local _source = source
     local xPlayer = TPZ.GetPlayer(_source)
@@ -46,6 +50,38 @@ local function HasPedTargetClose(closestEntityPeds, targetEntityId)
 
 end
 
+local function GenerateRandomRewards()
+
+    local maxRewards = math.random(Config.RandomRewards.MaximumRewards.min, Config.RandomRewards.MaximumRewards.max)
+
+    if maxRewards <= 0 then -- In case somehow maxRewards are 0, we return as null.
+        return nil
+    end
+
+    local rewardsList = {}
+
+    for i = 1, maxRewards do
+
+        local randomItem = Config.RandomRewards.Rewards[math.random(#Config.RandomRewards.Rewards)]
+
+        if rewardsList[randomItem.name] == nil then
+
+            local chance = math.random(1, 99)
+
+            if chance <= randomItem.chance then
+                rewardsList[randomItem.name] = {}
+                rewardsList[randomItem.name] = randomItem
+
+            end
+
+        end
+
+    end
+
+    return rewardsList
+
+end
+
 -----------------------------------------------------------
 --[[ Base Events  ]]--
 -----------------------------------------------------------
@@ -69,13 +105,10 @@ AddEventHandler("tpz_lootnpcs:server:reward", function(closestEntityPeds, entity
     local PlayerData       = GetPlayerData(_source)
     local xPlayer          = TPZ.GetPlayer(_source)
 
-    math.randomseed(os.time())
-    Wait(math.random(150, 350))
-
     -- It doesnt count as devtools, for some reason the loot prompt might be visible again, so we check only if the ped has been looted
     -- by the player source.
     if ListedEntities[entityId] then
-        SendNotification(nil, Locales['ENTITY_HAS_BEEN_ALREADY_LOOTED'], 'error')
+        SendNotification(_source, Locales['ENTITY_HAS_BEEN_ALREADY_LOOTED'], 'error')
         return
     end
 
@@ -93,12 +126,54 @@ AddEventHandler("tpz_lootnpcs:server:reward", function(closestEntityPeds, entity
         return
     end
 
-    print('entity exists, we give rewards')
+    local getRandomRewards = GenerateRandomRewards() -- table.
+
+    local received = nil
+
+    for index, reward in pairs (getRandomRewards) do
+
+        if reward.name == '0' or reward.name == '1' or reward.name == '2' then -- reward is account
+            local quantity = math.random(reward.quantity.min, reward.quantity.max)
+
+            xPlayer.addAccount(tonumber(reward.name), quantity)
+            local notify = not received and quantity .. ' ' .. reward.label or received .. ', ' .. quantity .. ' ' .. reward.label
+            received = notify
+
+        elseif StartsWith(reward.name, 'WEAPON_') then -- reward is weapon
+
+            local canCarryWeapon = xPlayer.canCarryWeapon(string.upper(reward.name) )
+
+            if canCarryWeapon then
+                xPlayer.addWeapon(string.upper(reward.name) )
+
+                local notify = not received and 'X1' .. reward.label or received .. ', X1 ' .. reward.label
+                received = notify
+            end
+
+        else -- reward is item
+
+            local quantity     = math.random(reward.quantity.min, reward.quantity.max)
+            local canCarryItem = xPlayer.canCarryItem(reward.name, quantity)
+
+            if canCarryItem then
+                xPlayer.addItem(reward.name, quantity)
+
+                local notify = not received and quantity .. ' ' .. reward.label or received .. ', ' .. quantity .. ' ' .. reward.label
+                received = notify
+            end
+
+        end
+
+    end
+
+    if received then
+        SendNotification(_source, string.format(Locales['RECEIVED_LOOT'], received), 'success')
+    end
 
     local WebhookData = Config.Webhooks['LOOTED']
 
     if WebhookData.Enabled then
-        local description = string.format('The specified user has looted an npc and received %s %s' )
+        local description = string.format('The specified user has looted an npc and received %s', received )
         TPZ.SendToDiscordWithPlayerParameters(WebhookData.Url, WebhookData.Title, _source, PlayerData.steamName, PlayerData.username, PlayerData.identifier, PlayerData.charIdentifier, description, WebhookData.Color)
     end
     
