755c1a2f1c92f9a4c88aab92a15d7c195cbf1516
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-16 21:50:59 +0200

    Update locales.lua

--- tpz_lootnpcs/locales.lua
+++ tpz_lootnpcs/locales.lua
@@ -2,6 +2,8 @@ Locales = {}
 
 Locales = {
 
+    ["ENTITY_HAS_BEEN_ALREADY_LOOTED"]        = "~e~The npc has been already looted",
+    ["RECEIVED_LOOT"]                           = "You received: %s",
     ['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'] = "DevTools / Injection Cheat Found!",
     ['DEVTOOLS_INJECTION_DETECTED']           = "You have been kicked due to cheating by using DevTools or Injection cheat.",
 }
